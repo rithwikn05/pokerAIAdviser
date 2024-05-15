a0790059a3c0c8fb7e1e04bc831c4b6aa9e47178
Rithwik  Nukala <rithwikn05@gmail.com>
2024-05-14 23:02:08 -0700

    dkfjfd

--- callsToGPT.py
+++ callsToGPT.py
@@ -4,5 +4,21 @@ class Implementation:
         self.output_label = ""
         self.system_input = '''You are a generator expert. Given a certain poker hand and
         context of the game, you are to advise the player on possible hands and considerations'''
-    def call():
-        print('herte') 
+
+    def generate(self):
+        my_prompt = self.prompt_field.name
+        api_key = ""
+        url = "https://api.openai.com/v1/chat/completions"
+        headers = {"Authorization":f"Bearer {api_key}"}
+        parameters = {
+            "model": "gpt-4o",
+            "messages": [
+                {"role": "system", "content": self.system_input},
+                {"role": "user", "content": my_prompt}
+            ],
+        }
+        response = requests.post(url, headers = headers, json = parameters)
+        response_data = response.json()
+        if response.status_code == 200:
+            generated_text = response_data["choices"][0]["messages"]["content"]
+            self.output_label = generated_text
