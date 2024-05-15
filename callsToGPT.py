29a2291a9e342125f226ab3a6122b1ef803e41ee
Rithwik  Nukala <rithwikn05@gmail.com>
2024-05-14 22:50:18 -0700

    slfjoi

---
+++ callsToGPT.py
@@ -0,0 +1,7 @@
+class Implementation:
+    def __init__(self):
+        self.prompt_field = ""
+        self.output_label = ""
+        self.system_input = '''You are a generator expert. Given a certain poker hand and
+        context of the game, you are to advise the player on possible hands and considerations'''
+    
