a01bd0c29daf465678e2e53e07d391dd317f0d87
Philip Chiu <philipc@berkeley.edu>
2024-05-23 20:06:48 -0700

    two buttons work

--- public/script.js
+++ public/script.js
@@ -1,12 +1,33 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const suitSelect = document.getElementById('suit');
-    const valueSelect = document.getElementById('value');
+    const card1Suit = document.getElementById('card1Suit');
+    const card1Value = document.getElementById('card1Value');
+    const card2Suit = document.getElementById('card2Suit');
+    const card2Value = document.getElementById('card2Value');
+    const inputText = document.getElementById('userInput');
     const output = document.getElementById('output');
 
-    window.showText = async function() {
-        const selectedSuit = suitSelect.value;
-        const selectedValue = valueSelect.value;
-        const userInput = `${selectedValue} of ${selectedSuit}s`;  // Format the user input as "Value of Suits"
+    window.showTextHand = async function() {
+        const card1 = `${card1Value.value} of ${card1Suit.value}s`;
+        const card2 = `${card2Value.value} of ${card2Suit.value}s`;
+        const userInput = `Card 1: ${card1}, Card 2: ${card2}`;
+        const response = await fetch('http://127.0.0.1:5000/generate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: userInput
+        });
+        if (response.ok) {
+            const data = await response.text();
+            output.textContent = data || 'hello.';
+        } else {
+            output.textContent = 'An error occurred.';
+        }
+    };
+
+
+    window.showTextGeneral = async function() {
+        const userInput = inputText.value; // Retrieve the value from the input field
         const response = await fetch('http://127.0.0.1:5000/generate', {
             method: 'POST',
             headers: {
